41adf058abfdb1d47689a75b8fd014befd85b735
sibtain.raza <sibtain.raza@10pearls.com>
2022-03-16 18:05:22 +0500

    Heroku Database URL Correction

--- app.py
+++ app.py
@@ -8,9 +8,16 @@ from resources.item import Items, ItemList
 from datetime import timedelta
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    'DATABASE_URL', 'sqlite:///data.db'
-)
+
+uri = os.getenv("DATABASE_URL")
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+if uri:
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = "smsrn123"  # Change this!
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=5)
